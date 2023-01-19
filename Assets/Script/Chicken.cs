926988db189ebacfe56d2cb7e0308917db38cbea
Solenn PROVOST <solenn.provost@ausy.fr>
2023-01-19 11:10:46 +0100

    implement method script chicken

--- Assets/Script/Chicken.cs
+++ Assets/Script/Chicken.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Chicken : MonoBehaviour
 {
     // Start is called before the first frame update
+    {
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    private float speed;
+
     void Start()
     {
-        
+        float = Random.Range(1.5f, 3f);
+        transform.position = startPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+        if (transform.position == endPosition)
+        {
+            Destroy(gameObject);
+        }
     }
+}
 }
